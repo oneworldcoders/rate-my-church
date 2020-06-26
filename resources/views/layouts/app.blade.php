fe15409a848b046b399a7a6b0680c6f492545c04
Emmanuel Omona <emmanuelomona1@gmail.com>
2020-06-26 10:59:44 +0200

    adds UI/UX components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Rate My Church') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,8 +24,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/home') }}">
+                    {{ config('app.name', 'Logo') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -74,6 +74,10 @@
         </nav>
 
         <main class="py-4">
+        <div class="jumbotron text-center slider">
+            <h1>{{ config('app.name', 'Rate My Church')  }} </h1>
+            <p>Find out what people think about your church?</p> 
+        </div>
             @yield('content')
         </main>
     </div>
--- resources/views/pages/admin/question/create.blade.php
+++ resources/views/pages/admin/question/create.blade.php
@@ -1,55 +1,78 @@
-@extends('pages.admin.question.layout')
+@extends('layouts.app')
 
 @section('content')
 
-@if ($errors->any())
-  <div class="alert alert-danger col-xs-9 col-sm-9 col-md-9">
-    <strong>Warning!</strong> Invalid Input <br><br>
-      <ul>
-        @foreach ($errors->all() as $error)
-          <li>{{ $error }}</li>
-        @endforeach
-      </ul>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+
+      @if ($errors->any())
+        <div class="alert alert-danger col-xs-9 col-sm-9 col-md-9">
+          <strong>Warning!</strong> Invalid Input <br><br>
+          <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+      @endif
+
+      <div class="card">
+        <div class="card-header">{{ __('Add a Question') }}</div>
+
+          <div class="card-body">
+            <form action="{{ route('questions.store') }}" method="POST">
+              @csrf
+
+              <div class="form-group row">
+                <label for="church_id" class="col-md-4 col-form-label text-md-right">{{ __('Church') }}</label>
+                <div class="col-md-6">
+                  <select name="church_id" id="church_id" class="form-control">
+                    <option value="" disabled>Choose a Church</option>
+                    @foreach ($churches as $church)
+                      <option value=" {{ $church->id }} ">{{ $church->name }}</option>
+                    @endforeach
+                  </select>
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>
+                <div class="col-md-6">
+                  <input type="text" name="title" class="form-control" placeholder="Question Title">
+                </div>
+              </div>
+
+              <div class="form-group row">
+                <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Descriprion') }}</label>
+                <div class="col-md-6">
+                  <textarea type="text" name="description" class="form-control" placeholder="Description"></textarea>
+                </div>
+              </div>
+
+              <div class="form-group row">
+              <label for="type" class="col-md-4 col-form-label text-md-right">{{ __('Type') }}</label>
+                <div class="col-md-6">
+                  <select name="type" id="type" class="form-control">
+                    <option value="" disabled>Type of Question</option>
+                    <option value="rate">Rating</option>
+                  </select>
+                </div>
+              </div>
+
+              <div class="form-group row mb-0">
+                <div class="col-md-6 offset-md-4">
+                  <button type="submit" class="btn btn-primary">
+                    {{ __('Add Question') }}
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-@endif
-
-<div class="col-xs-9 col-sm-9 col-md-9">
-  <form action="{{ route('questions.store') }}" method="POST">
-    @csrf
-    <fieldset class=""><legend>Add a Question</legend></fieldset>
-
-    <div class="form-group">
-      <strong>Church:</strong>
-      <select name="church_id" id="church_id" class="form-control">
-        <option value="" disabled>Choose a Church</option>
-        @foreach ($churches as $church)
-          <option value=" {{ $church->id }} ">{{ $church->name }}</option>
-        @endforeach
-      </select>
-    </div>
-
-    <div class="form-group">
-      <strong>Title:</strong>
-      <input type="text" name="title" class="form-control" placeholder="Question Title">
-    </div>
-
-    <div class="form-group">
-      <strong>Description:</strong>
-      <input type="text" name="description" class="form-control" placeholder="Description">
-    </div>
-
-    <div class="form-group">
-      <strong>Type:</strong>
-      <select name="type" id="type" class="form-control">
-        <option value="" disabled>Type of Question</option>
-        <option value="rate">Rating</option>
-      </select>
-    </div>
-
-    <div class=" text-center">
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </div> 
-  </form>
+  </div>
 </div>
 
 @endsection
